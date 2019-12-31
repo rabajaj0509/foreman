82e7476c46d1a4a1c364d267d8186a1ba3bb3f28
laviro <1ronlavi@gmail.com>
2019-12-31 12:35:12 +0200

    initial auth sources env

--- webpack/assets/javascripts/react_app/routes/routes.js
+++ webpack/assets/javascripts/react_app/routes/routes.js
@@ -1,5 +1,6 @@
 import HostWizard from './HostWizard';
 import Statistics from './Statistics';
 import Audits from './Audits';
+import AuthSources from './AuthSources';
 
-export const routes = [HostWizard, Statistics, Audits];
+export const routes = [HostWizard, Statistics, Audits, AuthSources];
